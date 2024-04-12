a9fc340139a02a6d3161617a2b53e6acb9ea1847
SAGR2003 <sagr20032006@gmail.com>
2024-04-12 18:32:02 -0500

    Add register functionality

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-const mongoose = require('mongoose');
+const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRoutes);
 
+connectDB();
+
 const PORT = process.env.PORT || 3000;
-
-const mongoURI = 'mongodb+srv://admin:4Rc9Sn6OCz5S7Mcv@marketplace.yp9n9dg.mongodb.net/?retryWrites=true&w=majority&appName=MarketPlace';
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Connection to MongoDB Succesful!'))
-.catch(err => console.error('Connection error to MongoDB:', err));
-
-
 app.listen(PORT, () => {
   console.log(`Server listening on the port ${PORT}`);
 });
---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://admin:4Rc9Sn6OCz5S7Mcv@marketplace.yp9n9dg.mongodb.net/?retryWrites=true&w=majority&appName=MarketPlace';
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(mongoURI);
+		console.log('Connection Succesful to MongoDB');
+	} catch (err) {
+		console.error('Connection error to MongoDB:', err);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
