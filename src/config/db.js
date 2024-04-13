de3f5c4d11d108ff4f10f0d5d0165e21c717bf6d
SAGR2003 <sagr20032006@gmail.com>
2024-04-12 21:30:57 -0500

    Update db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://admin:4Rc9Sn6OCz5S7Mcv@marketplace.yp9n9dg.mongodb.net/?retryWrites=true&w=majority&appName=MarketPlace';
+const mongoURI = 'mongodb+srv://admin:4Rc9Sn6OCz5S7Mcv@marketplace.yp9n9dg.mongodb.net/marketplace?retryWrites=true&w=majority&appName=MarketPlace';
 
 const connectDB = async () => {
 	try {
