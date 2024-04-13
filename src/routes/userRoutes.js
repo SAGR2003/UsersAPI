fb14da54701834996cf45e509928baaf0ae55f84
SAGR2003 <sagr20032006@gmail.com>
2024-04-12 19:13:12 -0500

    Add hashing for passwords

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
