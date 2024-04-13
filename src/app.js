934ece5b449c501615ef4ecd4f2f3e65ed9f2232
SAGR2003 <sagr20032006@gmail.com>
2024-04-12 22:48:31 -0500

    Update cors config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const cors = require('cors')
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
 
 connectDB();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
   console.log(`Server listening on the port ${PORT}`);
 });
